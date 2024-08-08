baa1ecb23d281a38cb85799bc6bec104ba8878b8
Esrog <98402018+Esrog@users.noreply.github.com>
2024-08-09 01:15:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,32 @@
-import os, math, logging, datetime, pytz
+import os
+import math
+import logging
+import datetime
+import pytz
 import logging.config
 
 from pyrogram.errors import BadRequest, Unauthorized
-from pyrogram import Client
-from pyrogram import types
+from pyrogram import Client, types
 
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
-from utils import temp, __repo__, __license__, __copyright__, __version__
+from utils import temp, repo, license, copyright, version
 from typing import Union, Optional, AsyncGenerator
 
 from plugins import web_server 
 from aiohttp import web
 
-# Get logging configurations
+# Configure logging
 logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("cinemagoer").setLevel(logging.ERROR)
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger(name)
 
 class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
+    def init(self):
+        super().init(
             name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
@@ -51,10 +53,12 @@ class Bot(Client):
         self.uptime = UPTIME
         curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
-        tame = curr.strftime('%I:%M:%S %p')
-        logger.info(LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__))
-        try: await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, tame, __repo__, __version__, __license__, __copyright__), disable_web_page_preview=True)   
-        except Exception as e: logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
+        time = curr.strftime('%I:%M:%S %p')
+        logger.info(LOG_MSG.format(me.first_name, date, time, repo, version, license, copyright))
+        try:
+            await self.send_message(LOG_CHANNEL, text=LOG_MSG.format(me.first_name, date, time, repo, version, license, copyright), disable_web_page_preview=True)   
+        except Exception as e:
+            logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
         if WEB_SUPPORT:
             app = web.AppRunner(await web_server())
             await app.setup()
@@ -63,21 +67,19 @@ class Bot(Client):
             
     async def stop(self, *args):
         await super().stop()
-        logger.info(f"Bot Is Restarting ⟳...")
+        logger.info("Bot Is Restarting ⟳...")
 
     async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
         current = offset
-        while True:
+        while current < limit:
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff)))
             for message in messages:
                 yield message
                 current += 1
 
-
-        
 Bot().run()
 
 
